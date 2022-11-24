7c86595282f7c5ae97ff4b9d2d0e9f09cde7fcec
Hamit <hamitsehjal26@gmail.com>
2022-11-23 21:49:56 -0500

    Added a "App.js" to keep record of "react code"

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// Here's our react code
+
+
+const App = () => {
+    return React.createElement("div", {}, React.createElement("h1", {}, "Adopt Me"))
+}
+
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"))
