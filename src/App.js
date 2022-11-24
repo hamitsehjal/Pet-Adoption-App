46431cfe04cebbc58c67a8e1d4256af41f75e0e6
Hamit <hamitsehjal26@gmail.com>
2022-11-23 22:03:58 -0500

    Added a Pet container!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 // Here's our react code
 
-
+// Pet container
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed)
+    ])
+}
 const App = () => {
-    return React.createElement("div", {}, React.createElement("h1", {}, "Adopt Me"))
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Adopt Me"),
+        React.createElement(Pet, {
+            name: "Luna",
+            animal: "Dog",
+            breed: "Havanese"
+        }),
+        React.createElement(Pet, {
+            name: "Pepper",
+            animal: "Bird",
+            breed: "Cockateil"
+        }),
+        React.createElement(Pet, {
+            name: "Doink",
+            animal: "Cat",
+            breed: "Mixed-Breed"
+        })])
 }
 
 
