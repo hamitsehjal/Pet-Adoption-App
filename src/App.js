425b43dee04be4ae44cbf8986f9816edfcc2010f
Hamit <hamitsehjal26@gmail.com>
2022-11-23 23:32:45 -0500

    Installed npm package and prettier

--- src/App.js
+++ src/App.js
@@ -5,28 +5,29 @@ const Pet = (props) => {
     return React.createElement("div", {}, [
         React.createElement("h1", {}, props.name),
         React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed)
-    ])
+        React.createElement("h2", {}, props.breed),
+    ]);
 }
+
 const App = () => {
     return React.createElement("div", {}, [
         React.createElement("h1", {}, "Adopt Me"),
         React.createElement(Pet, {
             name: "Luna",
             animal: "Dog",
-            breed: "Havanese"
+            breed: "Havanese",
         }),
         React.createElement(Pet, {
             name: "Pepper",
             animal: "Bird",
-            breed: "Cockateil"
+            breed: "Cockateil",
         }),
         React.createElement(Pet, {
             name: "Doink",
             animal: "Cat",
-            breed: "Mixed-Breed"
-        })])
-}
+            breed: "Mixed-Breed",
+        }),
+    ]);
+};
 
-
-ReactDOM.render(React.createElement(App), document.getElementById("root"))
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
